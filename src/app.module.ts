bae4f4348c36e752cbb85abfdcabb016a2ecad94
chessman <cjb15770595346@163.com>
2024-06-24 23:11:29 +0800

    init infra console

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AssetsModule } from './assets/assets.module';
+import { LocalStorageService } from './localStorage/localStorage.service';
+import { MasterModule } from './master/master.module';
+import { SyncService } from './sync/sync.service';
+import { UtilsService } from './utils/utils.service';
 
 @Module({
-  imports: [],
+  imports: [MasterModule, AssetsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SyncService, LocalStorageService, UtilsService],
 })
 export class AppModule {}
